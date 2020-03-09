dc54e78190af9aee14317d7e1c3dedf1547a821e
vika <v.sapunkova@neurobotics.ru>
2020-03-09 21:50:01 +0300

    initial commit

---
+++ statistics/src/main/java/ru/netology/stats/StatisticsService.java
@@ -0,0 +1,82 @@
+package ru.netology.stats;
+
+import java.util.Arrays;
+
+public class StatisticsService {
+        public long calculateSum(long[] sales) {
+        long sum = 0;
+            for (long sale : sales) {
+            // аналог sum = sum + purchase;
+            sum += sale;
+            }
+        return sum;
+        }
+
+        public long calculateAverage(long[] sales) {
+            long sum = 0;
+            long average = 0;
+            for (long sale : sales) {
+                // аналог sum = sum + sale;
+                sum += sale;
+                average = sum / 12;
+            }
+            return average;
+        }
+
+        public long findMax(long[] sales) {
+        long currentMax = sales[0];
+        long monthNumber = 0;
+        long resultMonth = 0;
+            for (long sale : sales) {
+            monthNumber += 1;
+                if (currentMax <= sale) {
+                currentMax = sale;
+                resultMonth = monthNumber;
+                }
+            }
+        return resultMonth;
+        }
+
+        public long findMin(long[] sales) {
+        long currentMin = sales[0];
+        long monthNumber = 0;
+        long resultMonth = 0;
+            for (long sale : sales) {
+            monthNumber += 1;
+                if (currentMin >= sale) {
+                currentMin = sale;
+                resultMonth = monthNumber;
+                }
+            }
+        return resultMonth;
+        }
+
+        public long calculateBelowAverage(long[] sales) {
+        long resultMonths = 0;
+        long sum = 0;
+        long average = 0;
+            for (long sale : sales) {
+            sum = Arrays.stream(sales).sum();
+            average = sum / 12;
+                if (sale < average) {
+                resultMonths += 1;
+                }
+            }
+        return resultMonths;
+        }
+
+        public long calculateAboveAverage(long[] sales) {
+        long resultMonths = 0;
+        long sum = 0;
+        long average = 0;
+            for (long sale : sales) {
+            sum = Arrays.stream(sales).sum();
+            average = sum / 12;
+                if (sale > average) {
+                resultMonths += 1;
+                }
+            }
+        return resultMonths;
+        }
+
+}
---
+++ statistics/src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -0,0 +1,82 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    @Test
+    void calculateSum() {
+        StatisticsService service = new StatisticsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+
+        long actual = service.calculateSum(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateAverage() {
+        StatisticsService service = new StatisticsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+
+        long actual = service.calculateAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+
+        long actual = service.findMax(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+
+        long actual = service.findMin(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateBelowAverage() {
+        StatisticsService service = new StatisticsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+
+        long actual = service. calculateBelowAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateAboveAverage() {
+        StatisticsService service = new StatisticsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+
+        long actual = service. calculateAboveAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    }
+
