b1b488db23affb508a84f8474113aa0dde6c8e0d
Viktoria <v.sapunkova@neurobotics.ru>
2020-03-13 16:31:40 +0300

    1. added long length into calculateAverage 2. changed initialization of long average 3. "sales" is placed to the beginning 4. Reformat Code

--- statistics/src/main/java/ru/netology/stats/StatisticsService.java
+++ statistics/src/main/java/ru/netology/stats/StatisticsService.java
@@ -1,83 +1,71 @@
 package ru.netology.stats;
 
-import java.util.Arrays;
-
 public class StatisticsService {
-        public long calculateSum(long[] sales) {
+    public long calculateSum(long[] sales) {
         long sum = 0;
-            for (long sale : sales) {
-            // аналог sum = sum + purchase;
+        for (long sale : sales) {
             sum += sale;
-            }
+        }
         return sum;
-        }
+    }
 
-        public long calculateAverage(long[] sales) {
-            long sum = 0;
-            long average = 0;
-            long length = 12;
-            for (long sale : sales) {
-                // аналог sum = sum + sale;
-                sum += sale;
-                average = sum / length;
-            }
-            return average;
+    public long calculateAverage(long[] sales) {
+        long sum = calculateSum(sales);
+        long length = 0;
+        for (long sale : sales) {
+            length += 1;
         }
+        return sum / length;
+    }
 
-        public long findMax(long[] sales) {
+    public long findMax(long[] sales) {
         long currentMax = sales[0];
         long monthNumber = 0;
         long resultMonth = 0;
-            for (long sale : sales) {
+        for (long sale : sales) {
             monthNumber += 1;
-                if (currentMax <= sale) {
+            if (currentMax <= sale) {
                 currentMax = sale;
                 resultMonth = monthNumber;
-                }
             }
-        return resultMonth;
         }
+        return resultMonth;
+    }
 
-        public long findMin(long[] sales) {
+    public long findMin(long[] sales) {
         long currentMin = sales[0];
         long monthNumber = 0;
         long resultMonth = 0;
-            for (long sale : sales) {
+        for (long sale : sales) {
             monthNumber += 1;
-                if (currentMin >= sale) {
+            if (currentMin >= sale) {
                 currentMin = sale;
                 resultMonth = monthNumber;
-                }
             }
+        }
         return resultMonth;
-        }
+    }
 
-        public long calculateBelowAverage(long[] sales) {
+    public long calculateBelowAverage(long[] sales) {
         long resultMonths = 0;
-        long sum = 0;
-        long average = 0;
-            for (long sale : sales) {
-            sum = Arrays.stream(sales).sum();
-            average = sum / 12;
-                if (sale < average) {
+        long average = calculateAverage(sales);
+        for (long sale : sales) {
+            if (sale < average) {
                 resultMonths += 1;
-                }
             }
-        return resultMonths;
         }
+        return resultMonths;
+    }
 
-        public long calculateAboveAverage(long[] sales) {
+    public long calculateAboveAverage(long[] sales) {
         long resultMonths = 0;
-        long sum = 0;
-        long average = 0;
-            for (long sale : sales) {
-            sum = Arrays.stream(sales).sum();
-            average = sum / 12;
-                if (sale > average) {
+        long average = calculateAverage(sales);
+        for (long sale : sales) {
+            if (sale > average) {
                 resultMonths += 1;
-                }
             }
-        return resultMonths;
         }
-
+        return resultMonths;
+    }
 }
+\n
